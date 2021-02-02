1260d67606178a3df9aee10357b8533bd1128957
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-03 01:19:23 +0200

    - Pages controller added to perofrom CRUD operations
    - Views added regarding those CRUD operations
    
    - Site.js modified to hold a confirmation box for deletion and a time out fade out
    
    - at the startup added in addition the areas endpoints
    
    Share folder added to the Area too

--- CmsStore/Startup.cs
+++ CmsStore/Startup.cs
@@ -52,6 +52,10 @@ namespace CmsStore
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
