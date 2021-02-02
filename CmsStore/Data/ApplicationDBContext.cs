6288e8c220262786930fc23c755f3e835233ae04
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-02 20:23:08 +0200

    - ApplicationDBContext added
    - Page Model class added
    
    - Connection string added
    
    - DBcontext configuration at the startup configuration done

---
+++ CmsStore/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CmsStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsStore.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
