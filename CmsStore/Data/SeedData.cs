d03a6a763890b29d664d89a835b3ab4b19781c60
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-02 20:46:22 +0200

    - Added SeedData class in order to be used to seed the database
    - At the program.cs the Main modified in order to seed the DataBase

---
+++ CmsStore/Data/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using CmsStore.Data;
+using CmsStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDBContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDBContext>>()))
+            {
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+
+                context.Pages.AddRange(
+                    new Page
+                    {
+                        Title = "Home",
+                        Slug = "home",
+                        Content = "home page",
+                        Sorting = 0
+                    },
+                    new Page
+                    {
+                        Title = "About Us",
+                        Slug = "about-us",
+                        Content = "about us page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Services",
+                        Slug = "services",
+                        Content = "services page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Contact",
+                        Slug = "contact",
+                        Content = "contact page",
+                        Sorting = 100
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
