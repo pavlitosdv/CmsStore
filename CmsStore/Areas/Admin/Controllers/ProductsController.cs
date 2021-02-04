2847c70014c4c14fc501fb671e7e74c5b7060e9e
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-04 14:33:20 +0200

    - Create POST action method added to the Products controller (uploads also an image)
    default image added to the media/products folder

--- CmsStore/Areas/Admin/Controllers/ProductsController.cs
+++ CmsStore/Areas/Admin/Controllers/ProductsController.cs
@@ -62,6 +62,49 @@ namespace CmsStore.Areas.Admin.Controllers
             return View();
         }
 
-  
+        // POST /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+                }
+
+                                                //image upload procedure
+                string imageName = "noimage.png";
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+                product.Image = imageName;
+
+
+
+                context.Add(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
     }
 }
