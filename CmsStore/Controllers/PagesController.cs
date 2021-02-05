6a5652dcf2c3cafbe97f8af5fa5bc145f06ab3fc
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-05 13:56:41 +0200

    PageController adde to the main folder of Controllers (outside Areas folder) with its Page view

---
+++ CmsStore/Controllers/PagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsStore.Data;
+using CmsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsStore.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly ApplicationDBContext context;
+
+        public PagesController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        // GET / or /slug
+        public async Task<IActionResult> Page(string slug)
+        {
+            if (slug == null)
+            {
+                return View(await context.Pages.Where(x => x.Slug == "home").FirstOrDefaultAsync());
+            }
+
+            Page page = await context.Pages.Where(x => x.Slug == slug).FirstOrDefaultAsync();
+
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+    }
+}
--- CmsStore/Startup.cs
+++ CmsStore/Startup.cs
@@ -52,7 +52,11 @@ namespace CmsStore
 
             app.UseEndpoints(endpoints =>
             {
-
+                endpoints.MapControllerRoute(
+                  "pages",
+                  "{slug?}",
+                  defaults: new { controller = "Pages", action = "Page" }
+              );
                 endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
