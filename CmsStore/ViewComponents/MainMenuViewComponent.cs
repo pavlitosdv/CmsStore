05c8165b14ad8bb6a6471dc0d200de79a570c90f
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-05 14:47:11 +0200

    ViewComponent Added (MainMenu and its corresponding view and implementation in the _Layout)

---
+++ CmsStore/ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using CmsStore.Data;
+using CmsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsStore.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDBContext context;
+
+        public MainMenuViewComponent(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
