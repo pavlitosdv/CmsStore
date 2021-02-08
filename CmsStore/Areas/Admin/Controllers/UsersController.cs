308922befa896d56af68fd7f34880c0df29b64c6
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-02-08 14:19:57 +0200

    Roles controller and Users controller added to the Admin area
    Views for the correspondings action methods of the above controllers added
    
    RoleEdit class added to Model

---
+++ CmsStore/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsStore.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
---
+++ CmsStore/Models/RoleEdit.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsStore.Models
+{
+    public class RoleEdit
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+        public string RoleName { get; set; }
+        public string[] AddIds { get; set; }
+        public string[] DeleteIds { get; set; }
+    }
+}
